cae216ae76f1ea376f3c808b1da2cfbdf1262e97
AldiMohammadRizkiana <114101522+AldiMohammadRizkiana@users.noreply.github.com>
2023-01-10 05:50:42 +0700

    Delete application directory

--- application/views/dashboard.php
+++
@@ -1,6 +0,0 @@
-<div class="jumbotron bg-primary text-white">
-    <h4 class="my-0">Selamat Datang Di</h4>
-    <h1 class="display-4 my-0">Aplikasi Absensi Online</h1>
-    <hr class="my-4">
-    <p class="lead">Aplikasi absensi online berbasis website</p>
-</div>
